36ab113e59fdf79bfc33df8dc1d0e3055a941714
Minhyuk Kim <rhrlwld@gmail.com>
2025-05-22 02:06:36 +0900

    test: 특정 사용자의 포인트 충전/이용 내역 조회 controller 테스트 추가

--- src/test/java/io/hhplus/tdd/point/controller/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/controller/PointControllerTest.java
@@ -1,5 +1,7 @@
 package io.hhplus.tdd.point.controller;
 
+import io.hhplus.tdd.point.dto.PointHistory;
+import io.hhplus.tdd.point.dto.TransactionType;
 import io.hhplus.tdd.point.dto.UserPoint;
 import io.hhplus.tdd.point.service.PointService;
 import org.junit.jupiter.api.DisplayName;
@@ -8,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -39,6 +43,34 @@ public class PointControllerTest {
                 .andExpect(jsonPath("$.id").value(expectedUserPoint.id()))
                 .andExpect(jsonPath("$.point").value(expectedUserPoint.point()))
                 .andExpect(jsonPath("$.updateMillis").value(expectedUserPoint.updateMillis()))
+                .andDo(print());
+    }
+
+    @DisplayName("특정 ID를 가진 사용자의 포인트 충전/이용 내역을 조회한다.")
+    @Test
+    void history() throws Exception {
+        // given
+        long userId = 1L;
+        PointHistory chargeHistory = new PointHistory(1L, userId, 1000, TransactionType.CHARGE, System.currentTimeMillis());
+        PointHistory useHistory = new PointHistory(2L, userId, 1000, TransactionType.USE, System.currentTimeMillis());
+
+        List<PointHistory> expectedHistories = List.of(chargeHistory, useHistory);
+        when(pointService.getPointHistoryByUserId(userId)).thenReturn(expectedHistories);
+
+        // when
+        mockMvc.perform(get("/point/{id}/histories", userId))
+                //then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(chargeHistory.id()))
+                .andExpect(jsonPath("$[0].userId").value(chargeHistory.userId()))
+                .andExpect(jsonPath("$[0].amount").value(chargeHistory.amount()))
+                .andExpect(jsonPath("$[0].type").value(chargeHistory.type().toString()))
+                .andExpect(jsonPath("$[0].updateMillis").value(chargeHistory.updateMillis()))
+                .andExpect(jsonPath("$[1].id").value(useHistory.id()))
+                .andExpect(jsonPath("$[1].userId").value(useHistory.userId()))
+                .andExpect(jsonPath("$[1].amount").value(useHistory.amount()))
+                .andExpect(jsonPath("$[1].type").value(useHistory.type().toString()))
+                .andExpect(jsonPath("$[1].updateMillis").value(useHistory.updateMillis()))
                 .andDo(print());
     }
 }
