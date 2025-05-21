b778ec46e505755ad18936f762c28ade08e8179e
Minhyuk Kim <rhrlwld@gmail.com>
2025-05-22 01:03:53 +0900

    test: 특정 사용자의 포인트 충전/이용 내역 조회 테스트 추가

--- src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
@@ -1,6 +1,9 @@
 package io.hhplus.tdd.point.service;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.dto.PointHistory;
+import io.hhplus.tdd.point.dto.TransactionType;
 import io.hhplus.tdd.point.dto.UserPoint;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +12,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
@@ -16,6 +21,9 @@ import static org.mockito.Mockito.when;
 public class PointServiceTest {
     @Mock
     private UserPointTable userPointTable;
+
+    @Mock
+    private PointHistoryTable pointHistoryTable;
 
     @InjectMocks
     private PointService pointService;
@@ -34,5 +42,23 @@ public class PointServiceTest {
 
         // then
         assertEquals(expectedUserPoint, actualUserPoint);
+    }
+
+    @DisplayName("특정 사용자의 포인트 충전/이용 내역을 조회한다.")
+    @Test
+    void getPointHistoryByUserId() {
+        // given
+        long userId = 1L;
+        PointHistory chargeHistory = new PointHistory(1L, userId, 1000, TransactionType.CHARGE, System.currentTimeMillis());
+        PointHistory useHistory = new PointHistory(2L, userId, 500, TransactionType.USE, System.currentTimeMillis());
+
+        List<PointHistory> expectedHistories = List.of(chargeHistory, useHistory);
+        when(pointHistoryTable.selectAllByUserId(userId)).thenReturn(expectedHistories);
+
+        // when
+        List<PointHistory> actualHistories = pointService.getPointHistoryByUserId(userId);
+
+        // then
+        assertEquals(expectedHistories, actualHistories);
     }
 }
