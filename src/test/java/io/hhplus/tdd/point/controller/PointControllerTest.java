eacd5be2eedf1fc5b649e747276314a4b1eb1e8e
Minhyuk Kim <rhrlwld@gmail.com>
2025-05-22 00:44:17 +0900

    chore: 특정 ID의 사용자 조회 controller 테스트 검증 로직 수정

--- src/test/java/io/hhplus/tdd/point/controller/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/controller/PointControllerTest.java
@@ -2,17 +2,18 @@ package io.hhplus.tdd.point.controller;
 
 import io.hhplus.tdd.point.dto.UserPoint;
 import io.hhplus.tdd.point.service.PointService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PointController.class)
 public class PointControllerTest {
@@ -23,8 +24,9 @@ public class PointControllerTest {
     @MockBean
     private PointService pointService;
 
+    @DisplayName("특정 ID를 가진 사용자의 포인트를 조회한다.")
     @Test
-    void getPoint() throws Exception {
+    void point() throws Exception {
         // given
         long userId = 1L;
         UserPoint expectedUserPoint = new UserPoint(userId, 1000, System.currentTimeMillis());
@@ -34,6 +36,9 @@ public class PointControllerTest {
         mockMvc.perform(get("/point/{id}", userId))
                 // then
                 .andExpect(status().isOk())
-                .andExpect(content().json("{\"id\":1,\"point\":100,\"updateMillis\":" + expectedUserPoint.updateMillis() + "}"));
+                .andExpect(jsonPath("$.id").value(expectedUserPoint.id()))
+                .andExpect(jsonPath("$.point").value(expectedUserPoint.point()))
+                .andExpect(jsonPath("$.updateMillis").value(expectedUserPoint.updateMillis()))
+                .andDo(print());
     }
 }
