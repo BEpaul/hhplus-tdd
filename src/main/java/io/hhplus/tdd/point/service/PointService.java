d79a14e2c9aaaf2d27019bec6b7531725ba4d7ee
Minhyuk Kim <rhrlwld@gmail.com>
2025-05-22 01:12:04 +0900

    feat: 특정 사용자의 포인트 충전/이용 내역 조회 service 구현

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -1,17 +1,26 @@
 package io.hhplus.tdd.point.service;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.dto.PointHistory;
 import io.hhplus.tdd.point.dto.UserPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PointService {
 
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
 
-    public UserPoint getUserPointById(Long id) {
+    public UserPoint getUserPointById(long id) {
         return userPointTable.selectById(id);
+    }
+
+    public List<PointHistory> getPointHistoryByUserId(long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
     }
 }
