3c29a33932d0bd12ba75b02ab47e187c4d56ae24
Minhyuk Kim <rhrlwld@gmail.com>
2025-05-22 23:53:35 +0900

    feat: 특정 사용자의 포인트 충전 UserPoint entity 구현

--- src/main/java/io/hhplus/tdd/point/entity/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/entity/UserPoint.java
@@ -9,4 +9,12 @@ public record UserPoint(
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
     }
+
+    public UserPoint charge(long amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("충전하려는 포인트 값은 음수이거나 0일 수 없습니다.");
+        }
+
+        return new UserPoint(this.id, this.point + amount, System.currentTimeMillis());
+    }
 }
