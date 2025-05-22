ffb9ab5dbec07ff6f07a5658b3a744636cf53d51
Minhyuk Kim <rhrlwld@gmail.com>
2025-05-23 02:31:34 +0900

    feat: 특정 사용자의 포인트 충전 controller 구현

--- src/main/java/io/hhplus/tdd/point/controller/PointController.java
+++ src/main/java/io/hhplus/tdd/point/controller/PointController.java
@@ -39,15 +39,16 @@ public class PointController {
         return pointService.getPointHistoryByUserId(id);
     }
 
+
     /**
-     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
+     * 특정 사용자의 포인트 충전
      */
     @PatchMapping("{id}/charge")
     public UserPoint charge(
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return pointService.chargeUserPoint(id, amount);
     }
 
     /**
