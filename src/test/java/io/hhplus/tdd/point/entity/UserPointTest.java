7732d9ac77e602d796210d64a3d5e4dab0e7a50d
Minhyuk Kim <rhrlwld@gmail.com>
2025-05-22 23:55:07 +0900

    chore: 특정 사용자의 포인트 충전 테스트 예외 케이스 추가

--- src/test/java/io/hhplus/tdd/point/entity/UserPointTest.java
+++ src/test/java/io/hhplus/tdd/point/entity/UserPointTest.java
@@ -8,17 +8,22 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserPointTest {
 
-    @DisplayName("충전하려는 포인트 값이 음수인 경우 예외가 발생한다.")
+    @DisplayName("충전하려는 포인트 값이 음수이거나 0인 경우 예외가 발생한다.")
     @Test
     void chargeNegativePoint_shouldThrowsException() {
         // given
         long userId = 1L;
         long negativeAmount = -1000L;
+        long zeroAmount = 0L;
         UserPoint currentUserPoint = new UserPoint(userId, 5000L, System.currentTimeMillis());
 
         // when & then
         assertThrows(IllegalArgumentException.class, () -> {
             currentUserPoint.charge(negativeAmount);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            currentUserPoint.charge(zeroAmount);
         });
     }
 
