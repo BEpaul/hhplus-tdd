3fea9d0e05fe306eb8ce69addf9ff4a0653c627d
Minhyuk Kim <rhrlwld@gmail.com>
2025-05-23 02:00:07 +0900

    feat: 특정 사용자의 포인트 충전 service 구현

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -3,6 +3,7 @@ package io.hhplus.tdd.point.service;
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.point.entity.PointHistory;
+import io.hhplus.tdd.point.entity.TransactionType;
 import io.hhplus.tdd.point.entity.UserPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,15 @@ public class PointService {
 
     public List<PointHistory> getPointHistoryByUserId(long userId) {
         return pointHistoryTable.selectAllByUserId(userId);
+    }
+
+    public UserPoint chargeUserPoint(long userId, long amount) {
+        UserPoint userPoint = userPointTable.selectById(userId);
+        UserPoint chargedUserPoint = userPoint.charge(amount);
+
+        userPointTable.insertOrUpdate(userId, chargedUserPoint.point());
+        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        return chargedUserPoint;
     }
 }
