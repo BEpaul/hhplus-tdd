c72274704f61514eae0e94e0de65b4c35fbc564f
Minhyuk Kim <rhrlwld@gmail.com>
2025-05-23 06:37:39 +0900

    test: 특정 사용자의 포인트 사용 UserPoint entity 테스트 추가

--- src/test/java/io/hhplus/tdd/point/entity/UserPointTest.java
+++ src/test/java/io/hhplus/tdd/point/entity/UserPointTest.java
@@ -3,6 +3,7 @@ package io.hhplus.tdd.point.entity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -41,5 +42,57 @@ public class UserPointTest {
 
         // then
         assertEquals(expectedUserPoint.point(), userPoint.point());
+    }
+
+    @DisplayName("사용하려는 포인트 값이 음수이거나 0인 경우 예외가 발생한다.")
+    @Test
+    void useNegativePoint_shouldThrowsException() {
+        // given
+        long userId = 1L;
+        long negativeAmount = -1000L;
+        long zeroAmount = 0L;
+        UserPoint currentUserPoint = new UserPoint(userId, 5000L, System.currentTimeMillis());
+
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> {
+            currentUserPoint.use(negativeAmount);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            currentUserPoint.use(zeroAmount);
+        });
+    }
+
+    @DisplayName("사용하려고 하는 포인트가 보유 포인트보다 큰 경우 예외가 발생한다.")
+    @Test
+    void useMoreThanAvailablePoint_shouldThrowsException() {
+        // given
+        long userId = 1L;
+        long currentPoint = 5000L;
+        long useAmount = 8000L;
+        UserPoint userPoint = new UserPoint(userId, currentPoint, System.currentTimeMillis());
+
+        // when & then
+        assertThatThrownBy(() -> userPoint.use(useAmount))
+                .isInstanceOf(NotEnoughCurrentPointException.class)
+                .hasMessageContaining("사용하려는 포인트 값이 보유 포인트보다 큽니다.");
+    }
+
+    @DisplayName("사용하려는 포인트 값이 양수이고 보유 포인트보다 작은 경우 정상적으로 포인트를 사용한다.")
+    @Test
+    void usePoint() {
+        // given
+        long userId = 1L;
+        long currentPoint = 5000L;
+        long useAmount = 3000L;
+        long expectedPoint = currentPoint - useAmount;
+        UserPoint currentUserPoint = new UserPoint(userId, currentPoint, System.currentTimeMillis());
+
+        // when
+        UserPoint userPoint = currentUserPoint.use(useAmount);
+
+        // then
+        assertEquals(currentUserPoint.id(), userPoint.id());
+        assertEquals(expectedPoint, userPoint.point());
     }
 }
